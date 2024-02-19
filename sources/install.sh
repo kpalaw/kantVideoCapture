cf29acd5e5e14d0b948d8269f8bb89141e86911e
kpalaw <nongkhul@gmail.com>
2024-02-19 23:31:32 +0000

    dependencies module

---
+++ sources/install.sh
@@ -0,0 +1,7 @@
+echo "Installing dependencies"
+
+sudo apt-get install -y libavformat-dev
+sudo apt-get install -y libavcodec-dev
+sudo apt-get install -y libswscale-dev
+sudo apt-get install -y libsdl2-dev
+sudo apt-get install -y libpng-dev
